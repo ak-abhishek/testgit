5372a739ddadfee95c077b4670f8deeac0b7fdc6
ak-abhishek <ak.abhishekkalindi@gmail.com>
2018-02-05 23:30:08 -0800

    Adding person class# Please enter the commit message for your changes. Lines starting

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person{
+
+	private String firstName;
+	private String lastName;
+
+	public Person(String firstNmae, String lastName)
+	{
+		this.lastName=lastName;
+		this.firstName-firstName;
+		
+	}
+}
